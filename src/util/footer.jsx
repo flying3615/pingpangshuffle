095aeb55a56c4bd61be5c129b438c48752a52165
Yufei Liu <yufei.liu@sgdigital.com>
2019-07-01 17:32:16 +1200

    with heart

--- src/util/footer.jsx
+++ src/util/footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 
 const MadeWithLove = () => {
@@ -10,10 +10,10 @@ const MadeWithLove = () => {
     return (
      
             <Typography variant="body1" color="textSecondary" align="center">
-                {'Built with love by '}
+            {'Made with '} <FontAwesomeIcon icon={faHeart} color="red"/> {' by '}
                 <Link color="inherit" href="mailto:gabriel.liu3615@gmail.com">
                     Yufei
-            </Link>
+                </Link>
             </Typography>
        
     );
